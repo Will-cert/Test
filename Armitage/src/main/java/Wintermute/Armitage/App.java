e443bc30c1d2e7fe012b7c5bfb859988212d3c50
Chris Moser <cmoser@certon.com>
2017-11-13 12:36:24 -0500

    Testing this out

---
+++ Armitage/src/main/java/Wintermute/Armitage/App.java
@@ -0,0 +1,13 @@
+package Wintermute.Armitage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
